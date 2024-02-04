eff0c2e661639e25d19e83290622be59d9d8caee
Daleray1231 <daleray1231@gmail.com>
2024-02-04 12:44:37 -0600

    changed prompts to checkboxes for cleaner UI

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,31 +1,26 @@
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var passwordForm = document.querySelector("#passwordForm");
 
-// Write password to the #password input
 function writePassword() {
-  var passwordLength = prompt("Enter password length ( 8 - 128 ):");
+  var passwordLength = parseInt(passwordForm.elements.passwordLength.value);
 
-  // Validate the password length input
-  if (passwordLength !== null && passwordLength !== "") {
-    passwordLength = parseInt(passwordLength);
-    if (passwordLength >= 8 && passwordLength <= 128) {
-      var useUppercase = confirm("Include Uppercase letters?");
-      var useLowercase = confirm("Include Lowercase letters?");
-      var useNumbers = confirm("Include Numbers?");
-      var useSpecialCharacters = confirm("Include Special characters?");
-      var password = generatePassword(passwordLength, useUppercase, useLowercase, useNumbers, useSpecialCharacters);
-      var passwordText = document.querySelector("#password");
-      passwordText.value = password;
-    } else {
-      alert("Password length must be between 8 and 128 characters.");
-    }
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var useUppercase = passwordForm.elements.uppercase.checked;
+    var useLowercase = passwordForm.elements.lowercase.checked;
+    var useNumbers = passwordForm.elements.numbers.checked;
+    var useSpecialCharacters = passwordForm.elements.specialCharacters.checked;
+
+    var password = generatePassword(passwordLength, useUppercase, useLowercase, useNumbers, useSpecialCharacters);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  } else {
+    alert("Password length must be between 8 and 128 characters.");
   }
 }
 
-// Function to generate a random password
 function generatePassword(length, useUppercase, useLowercase, useNumbers, useSpecialCharacters) {
   var charset = "";
-  
+
   if (useLowercase) {
     charset += "abcdefghijklmnopqrstuvwxyz";
   }
@@ -38,16 +33,15 @@ function generatePassword(length, useUppercase, useLowercase, useNumbers, useSpe
   if (useUppercase) {
     charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  
+
   var password = "";
-  
+
   for (var i = 0; i < length; i++) {
     var randomIndex = Math.floor(Math.random() * charset.length);
     password += charset.charAt(randomIndex);
   }
-  
+
   return password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
